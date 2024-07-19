7f230976d30ff70bcd5087b056b6b9245b8733de
TechPriyamvad <priyamvadofficial1@gmail.com>
2024-07-19 11:16:09 +0530

    created UI for item and related items

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from  item.models import Item,Category
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,"core/index.html")
+    categories = Category.objects.all();
+    items = Item.objects.filter(is_sold=False)[0:6]
+
+    return render(request,"core/index.html",{
+        'items':items,
+        'categories':categories
+    })
 
 #view|controller for contact page
 def contact(request):
